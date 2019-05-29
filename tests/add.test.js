64788cf0b248e17739e1d8996a53201db45e94ed
atul-singh-xelp <atul.singh@xelpmoc.in>
2019-05-29 14:17:00 +0530

    dividemetho

--- tests/add.test.js
+++ tests/add.test.js
@@ -1,8 +1,10 @@
-const add = require('../add')
+const method = require('../add')
 
-test("check adding 2 numbers", ()=>{
-    const result = add(2,3);
-
-    expect(result).toBe(5);
-    
-})
+test("check adding 2 numbers", () => {
+    const result = method.add(2,3);
+    expect(result).toBe(5);    
+});
+test("check divide 2 numbers", () => {
+    const result = method.divide(10,5);
+    expect(result).toBe(3);
+});
